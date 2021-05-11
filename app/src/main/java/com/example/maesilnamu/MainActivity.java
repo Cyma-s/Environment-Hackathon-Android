8c9e1ddae57337fcf879ed11e4b6c32dda8252db
Eunsol Kim <ssol0319@naver.com>
2021-05-11 17:41:53 +0900

    add : LoginActivity, SignupActivity are created. MainActivity implementation is complete.

--- app/src/main/java/com/example/maesilnamu/MainActivity.java
+++ app/src/main/java/com/example/maesilnamu/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.maesilnamu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login_button;
+    private TextView signup_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_button = (Button) findViewById(R.id.login_button);
+        signup_button = (TextView) findViewById(R.id.signup_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
