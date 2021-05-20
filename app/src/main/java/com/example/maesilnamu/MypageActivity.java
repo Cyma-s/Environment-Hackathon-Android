29e5b56495d1f19746f7f8da68fd250b26fd6e5f
dmstjdhdh <dmsbow_@daum.net>
2021-05-21 05:26:56 +0900

    MYPAGE xml and create Activity and Class

--- app/src/main/java/com/example/maesilnamu/MypageActivity.java
+++ app/src/main/java/com/example/maesilnamu/MypageActivity.java
@@ -1,20 +1,32 @@
 package com.example.maesilnamu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MypageActivity extends AppCompatActivity {
+    private RecyclerView recyclerView1;
+    private RecyclerView recyclerView2;
+    private QuestlistAdapter adapter1;
+    private MyPostListAdapter adapter2;
+    private int currentSize1;
+    private int currentSize2;
+    private LinearLayoutManager layoutManager;
 
+    private boolean isLoading1 = false;
     private ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
         backButton = (ImageView) findViewById(R.id.back_button);
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,4 +36,5 @@ public class MypageActivity extends AppCompatActivity {
             }
         });
     }
+
 }
