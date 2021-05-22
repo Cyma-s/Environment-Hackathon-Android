ea1e6b00b422a9e8d1c69409c0714df88582ce7b
dmstjdhdh <dmsbow_@daum.net>
2021-05-22 11:58:25 +0900

    sidebar create

--- app/src/main/java/com/example/maesilnamu/HomeActivity.java
+++ app/src/main/java/com/example/maesilnamu/HomeActivity.java
@@ -1,13 +1,18 @@
 package com.example.maesilnamu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +27,7 @@ import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,12 +39,18 @@ import java.util.Map;
 public class HomeActivity extends AppCompatActivity {
     public String treeName, treeExp, maxExp, userExp, treeLevel, maxLevel, userToken;
     private Button missionConnect, writeConnect;
+    private ImageView sidebar;
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+    private ImageButton whiteground;
+    private Button mypagebutton, rankbutton, shopbutton, postbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         getBreedingInfo();
+
 
         // exp Up
         Button expUp = (Button) findViewById(R.id.expUp);
@@ -48,6 +60,14 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView = (View)findViewById(R.id.sidebar_open);
+        sidebar = (ImageView)findViewById(R.id.sidebar);
+        whiteground = (ImageButton)findViewById(R.id.backgroundwhite);
+        mypagebutton = (Button)findViewById(R.id.btn_mypage);
+        rankbutton = (Button)findViewById(R.id.btn_rank);
+        shopbutton = (Button)findViewById(R.id.btn_shop);
+        postbutton = (Button)findViewById(R.id.btn_post);
 
         missionConnect = (Button) findViewById(R.id.mission_connect);
         writeConnect = (Button) findViewById(R.id.mission_write);
@@ -185,5 +205,80 @@ public class HomeActivity extends AppCompatActivity {
         };
 
         queue.add(jsonObjectRequest);
+        sidebar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        whiteground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+        mypagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back_intent = new Intent(HomeActivity.this, MainActivity.class);
+                finish();
+                startActivity(back_intent);
+            }
+        });
+        rankbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back_intent = new Intent(HomeActivity.this, MainActivity.class);
+                finish();
+                startActivity(back_intent);
+            }
+        });
+        shopbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back_intent = new Intent(HomeActivity.this, MainActivity.class);
+                finish();
+                startActivity(back_intent);
+            }
+        });
+        postbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back_intent = new Intent(HomeActivity.this, MainActivity.class);
+                finish();
+                startActivity(back_intent);
+            }
+        });
     }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull @NotNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull @NotNull View drawerView) {
+
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull @NotNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
+
 }
