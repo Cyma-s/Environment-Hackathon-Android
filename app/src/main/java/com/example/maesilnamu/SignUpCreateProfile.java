92760fbdf2a995ce09731a09ad1a3091d1179a1c
Eunsol Kim <ssol0319@naver.com>
2021-06-01 00:35:03 +0900

    edit ( SignUpCreateProfile ) : 회원 가입시 사진 첨부 추가

--- app/src/main/java/com/example/maesilnamu/SignUpCreateProfile.java
+++ app/src/main/java/com/example/maesilnamu/SignUpCreateProfile.java
@@ -1,10 +1,17 @@
 package com.example.maesilnamu;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,18 +34,27 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SignUpCreateProfile extends AppCompatActivity {
     private Button signupButton;
-    private ImageView backButton, validityCheckButton;
+    private ImageView backButton, validityCheckButton, signup_user_image;
     private TextView nicknameConfigure;
     private EditText nicknameText, locationText;
     private Spinner locationSpin;
     private String nickname;
-
+    private final int CODE_ALBUM_REQUEST = 111;
+    private Bitmap bitmap;
+    private String bitmapString = "";
     private boolean checkNickname = false, checkLocation = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +68,7 @@ public class SignUpCreateProfile extends AppCompatActivity {
         nicknameText = (EditText) findViewById(R.id.signup_nickname);
         locationText = (EditText) findViewById(R.id.signup_location);
         locationSpin = (Spinner) findViewById(R.id.location_spinner);
+        signup_user_image = (ImageView) findViewById(R.id.signup_user_image);
 
 
         Intent prev_intent = getIntent();
@@ -61,53 +78,23 @@ public class SignUpCreateProfile extends AppCompatActivity {
         locationText.setBackgroundResource(R.drawable.blackborder);
         locationSpin.setBackgroundResource(R.drawable.blackborder);
 
+        signup_user_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        "image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivityForResult(intent, CODE_ALBUM_REQUEST);
+            }
+        });
+
+
+
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nicknameAuthentic = nickname;
-                String location = locationText.getText().toString();
-
-                System.out.println("checkNickname: " + checkNickname);
-                System.out.println("checkLocation: " + checkLocation);
-                System.out.println("location: " + location);
-                System.out.println("nickname: " + nicknameAuthentic);
-                System.out.println("email: " + emailText);
-                System.out.println("password: " + passwordText);
-
-                if(checkNickname && checkLocation) {
-                    JSONObject infoForSignup = new JSONObject();
-                    try {
-                        infoForSignup.put("eMail", emailText);
-                        infoForSignup.put("password", passwordText);
-                        infoForSignup.put("name", nicknameAuthentic);
-                        infoForSignup.put("image", "");
-                        infoForSignup.put("location", location);
-                        infoForSignup.put("exp", "0");
-                        infoForSignup.put("token", "50");
-                    } catch (JSONException exception) {
-                        exception.printStackTrace();
-                    }
-
-                    String url = getString(R.string.url) + "/user/signUp";
-                    RequestQueue queue = Volley.newRequestQueue(SignUpCreateProfile.this);
-                    final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, infoForSignup,
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-
-                                }
-                            }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-    //                        Toast.makeText(SignUpCreateProfile.this, "오류입니다", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                    queue.add(jsonObjectRequest);
-                    Intent intent = new Intent(SignUpCreateProfile.this, MainActivity.class);
-                    finish();
-                    startActivity(intent);
-                }
+                sendToServer(emailText, passwordText, bitmapString);
             }
         });
 
@@ -127,7 +114,7 @@ public class SignUpCreateProfile extends AppCompatActivity {
 
                 String url = getString(R.string.url) + "/user/validation/email/" + nickname;
 
-                System.out.println(url);
+                //System.out.println(url);
 
                 RequestQueue queue = Volley.newRequestQueue(SignUpCreateProfile.this);
                 final StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -145,7 +132,7 @@ public class SignUpCreateProfile extends AppCompatActivity {
                         }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(SignUpCreateProfile.this, "오류입니다.", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(SignUpCreateProfile.this, "오류입니다.", Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -166,5 +153,90 @@ public class SignUpCreateProfile extends AppCompatActivity {
                 locationText.setText("지역을 선택하세요.");
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CODE_ALBUM_REQUEST && resultCode == RESULT_OK && data != null){
+            if(data.getClipData() != null){
+                ClipData clipData = data.getClipData();
+                Uri filePath = clipData.getItemAt(0).getUri();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                    bitmapString = bitmapToString(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                signup_user_image.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    String getRealPathFromUri(Uri uri){
+        String[] proj = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(this, uri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        assert cursor != null;
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+
+    public String bitmapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 10, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String imageString = Base64.getEncoder().encodeToString(imageBytes);
+        return imageString;
+    }
+
+    private void sendToServer(String emailText, String passwordText, String bitmapString){
+        String nicknameAuthentic = nickname;
+        String location = locationText.getText().toString();
+/*
+        System.out.println("checkNickname: " + checkNickname);
+        System.out.println("checkLocation: " + checkLocation);
+        System.out.println("location: " + location);
+        System.out.println("nickname: " + nicknameAuthentic);
+        System.out.println("email: " + emailText);
+        System.out.println("password: " + passwordText); */
+
+        if(checkNickname && checkLocation) {
+            JSONObject infoForSignup = new JSONObject();
+            try {
+                infoForSignup.put("eMail", emailText);
+                infoForSignup.put("password", passwordText);
+                infoForSignup.put("name", nicknameAuthentic);
+                infoForSignup.put("image", bitmapString);
+                infoForSignup.put("location", location);
+                infoForSignup.put("exp", "0");
+                infoForSignup.put("token", "50");
+            } catch (JSONException exception) {
+                exception.printStackTrace();
+            }
+
+            String url = getString(R.string.url) + "/user/signUp";
+            RequestQueue queue = Volley.newRequestQueue(SignUpCreateProfile.this);
+            final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, infoForSignup,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(SignUpCreateProfile.this, "오류입니다", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            queue.add(jsonObjectRequest);
+            Intent intent = new Intent(SignUpCreateProfile.this, MainActivity.class);
+            finish();
+            startActivity(intent);
+        }
     }
 }
