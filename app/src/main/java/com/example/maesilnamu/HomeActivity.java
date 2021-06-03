d4c1a084e097533bec283cc74a97e4a3619ddd20
5061ksh@naver.com <5061ksh@naver.com>
2021-06-03 22:16:04 +0900

    edit(HomeActivity, activity_home.xml, activity_sidebar.xml) : 스토리 불러오는 홈 화면 기본 구성 완료

--- app/src/main/java/com/example/maesilnamu/HomeActivity.java
+++ app/src/main/java/com/example/maesilnamu/HomeActivity.java
@@ -35,58 +35,100 @@ import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeActivity extends AppCompatActivity {
-    public String treeName, treeExp, maxExp, userExp, treeLevel, maxLevel, userToken;
-    private Button missionConnect, writeConnect;
-    private ImageView sidebar;
+    private Button missionButton1, missionButton2, missionButton3;
+    private ImageView sidebar, storyImage;
     private DrawerLayout drawerLayout;
     private View drawerView;
     private ImageButton whiteground;
-    private TextView mypagebutton, rankbutton, shopbutton, postbutton;
+    private TextView mypagebutton, rankbutton, postbutton;
+    private TextView storyTitle, storyContent1, storyContent2;
+
+    private String storyTitleString = "Chapter 1 - 스토리 챕터 제목 or 번호를 추가하세요;";
+    private String storyString1 = "여기에는 스토리 내용을 추가해줘요";
+    private String storyString2 = "여기에는 스토리 내용을 더 넣거나 넣을 내용이 없으면 컴포넌트를 제거해줘요";
+    private String tempTitleString = "";
+    private String tempString1 = "";
+    private String tempString2 = "";
+    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //getBreedingInfo();
-
-
-        // exp Up
-        Button expUp = (Button) findViewById(R.id.expUp);
-        expUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         drawerView = (View)findViewById(R.id.sidebar_open);
         sidebar = (ImageView)findViewById(R.id.sidebar);
+        storyImage = (ImageView) findViewById(R.id.story_image);
         whiteground = (ImageButton)findViewById(R.id.backgroundwhite);
         mypagebutton = (TextView)findViewById(R.id.btn_mypage);
         rankbutton = (TextView)findViewById(R.id.btn_rank);
-        shopbutton = (TextView)findViewById(R.id.btn_shop);
         postbutton = (TextView)findViewById(R.id.btn_post);
+        storyTitle = (TextView) findViewById(R.id.story_title);
+        storyContent1 = (TextView) findViewById(R.id.story_content_1);
+        storyContent2 = (TextView) findViewById(R.id.story_content_2);
+        missionButton1 = (Button) findViewById(R.id.mission1);
+        missionButton2 = (Button) findViewById(R.id.mission2);
+        missionButton3 = (Button) findViewById(R.id.mission3);
 
-        missionConnect = (Button) findViewById(R.id.mission_connect);
-        writeConnect = (Button) findViewById(R.id.mission_write);
-        missionConnect.setOnClickListener(new View.OnClickListener() {
+        this.getStoryContents();
+
+        /** 여기서부터는 애니메이션으로 한 글자씩 스토리 내용 출력 */
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask(){
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (count >= storyString1.length() + storyString2.length())
+                            return;
+                        else if (count < storyString1.length()){
+                            tempString1 += storyString1.charAt(count);
+                            storyContent1.setText(tempString1);
+                            count++;
+                        }
+                        else {
+                            tempString2 += storyString2.charAt(count - storyString1.length());
+                            storyContent2.setText(tempString2);
+                            count++;
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 0, 100);
+
+
+        missionButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, MypageActivity.class);
-                //finish();
+                Intent intent = new Intent(HomeActivity.this, QuestLoadActivity.class);
+
                 startActivity(intent);
             }
         });
-        writeConnect.setOnClickListener(new View.OnClickListener() {
+        missionButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, QuestPostWriteActivity.class);
-                //finish();
+                Intent intent = new Intent(HomeActivity.this, QuestLoadActivity.class);
+
                 startActivity(intent);
             }
         });
+        missionButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, QuestLoadActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
         sidebar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -132,14 +174,6 @@ public class HomeActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
-            shopbutton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-                    finish();
-                    startActivity(intent);
-                }
-            });
             postbutton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -148,8 +182,6 @@ public class HomeActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
-
-
         }
 
         @Override
@@ -163,4 +195,33 @@ public class HomeActivity extends AppCompatActivity {
         }
     };
 
+    private void getStoryContents() {
+        String url = getString(R.string.url)+"스토리 정보 받는 이메일 주소";
+
+        System.out.println(url);
+
+        /** 미션 정보 받아오는 코드 삽입 */ /*
+        RequestQueue queue = Volley.newRequestQueue(StoryLoadActivity.this);
+        final StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });  */
+
+        /**받아온 미션 정보는 다음 변수에 저장 */
+        storyTitleString = "스토리 챕터 번호 or 챕터 제목";
+        storyString1 = "스토리 내용 텍스트 1";
+        storyString2 = "스토리 내용 텍스트 2";
+        storyTitle.setText(storyTitleString);
+
+        // 여기는 스토리 이미지를 불러와서 저장
+        storyImage.setImageResource(R.drawable.little_leaf);
+    }
 }
